cd9914a41c436f102523120400e48d893f5a1355
Mantu Nigam <mantuonweb@gmail.com>
2025-05-17 09:50:10 +0530

    feat: remove event-service
    This commit removes the event-service from the repository. The service's code, Dockerfile, configuration, routes, controllers, and dependencies have been deleted. The service has also been removed from the docker-compose configuration.

--- events-service/routes/events.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const eventController = require('../controllers/events.controller');
-
-const router = express.Router();
-
-// Event routes
-router.post('/sync', eventController.dispatchSync);
-
-module.exports = router;
